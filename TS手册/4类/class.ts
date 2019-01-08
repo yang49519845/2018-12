390ed70763d94a6119ab30ed70fa9365c1894cc8
yangxin <yangxin@chexd.com>
2019-01-08 15:20:27 +0800

    官网vuex购物车demo

--- TS手册/4类/class.ts
+++ TS手册/4类/class.ts
@@ -11,3 +11,39 @@ class Greeter {
 let greeter = new Greeter('world')
 
 // Inheritance 继承
+// class Animal {
+//   move(distanceInMeters: number = 0) {
+//     console.log(`Animal moved ${distanceInMeters}m.`)
+//   }
+// }
+// class Dog extends Animal {
+//   bark() {
+//     console.log('woof! woof!')
+//   }
+// }
+
+// const dog = new Dog()
+
+// dog.bark()
+// dog.move(10)
+// dog.bark()
+
+class Animal {
+  name: string
+  constructor(theName: string) {
+    this.name = theName
+  }
+  move(distanceInMeters: number = 0) {
+    console.log(`${this.name} moved ${distanceInMeters}`)
+  }
+}
+
+class Snake extends Animal {
+  constructor(name: string) {
+    super(name)
+  }
+  move(distanceInMeters = 5) {
+    console.log('Slithering...')
+    super.move(distanceInMeters)
+  }
+}
